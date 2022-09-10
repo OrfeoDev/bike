6e388d448efac597be2b9eb73a9c0338e07e150a
OrfeoDev <orfeo34@icloud.com>
2022-09-10 19:41:05 +0200

    Debut de creation du formulaire de creation de produit

--- orendhaBike/src/Controller/ProduitController.php
+++ orendhaBike/src/Controller/ProduitController.php
@@ -9,35 +9,53 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProduitController extends AbstractController
 {
     #[Route('/{slug}', name: 'app_produit_categorie')]
-    public function categorie($slug,CategorieRepository $categorieRepository): Response
+    public function categorie($slug, CategorieRepository $categorieRepository): Response
     {
-        $categorie = $categorieRepository->findOneBy(['slug'=>$slug]);
+        $categorie = $categorieRepository->findOneBy(['slug' => $slug]);
 
-        if (!$categorie){
+        if (!$categorie) {
             throw new NotFoundHttpException("la categorie n'existe pas ");
         }
         return $this->render('produit/categorie.html.twig', [
             'slug' => $slug,
-            'categorie'=> $categorie
+            'categorie' => $categorie
         ]);
     }
 
     /**
      * @Route("/{category_slug}/{slug}",name="product_show")
      */
-    public function show($slug,ProduitRepository $produitRepository)
+    public function show($slug, ProduitRepository $produitRepository, UrlGeneratorInterface $generator)
     {
-$produit = $produitRepository->findOneBy(['slug'=>$slug]);
-        if (!$produit){
+
+
+        $produit = $produitRepository->findOneBy(['slug' => $slug]);
+        if (!$produit) {
             // Ici avec le createNotFound me permet de lancer une excpetion indant que produit n'exsite pas
             throw $this->createNotFoundException("le produit demandé n'existe pas ");
         }
-        return $this->render('produit/show.html.twig',[
-            'produit'=>$produit
+        return $this->render('produit/show.html.twig', [
+            'produit' => $produit
         ]);
     }
+
+    /**
+     * @Route("/admin/produit/creation",name="creation_produit")
+     */
+    public function creation ()
+    {
+
+
+
+        return $this->render('produit/creation.html.twig', [
+
+        ]);
+    }
+
+
 }
